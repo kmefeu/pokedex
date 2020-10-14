49dd1c2a2a9e44dc5d05c9ad75c63c1ede6dc1a0
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-10-14 17:19:28 -0300

    Day#2 #Up#4

--- color.js
+++ color.js
@@ -1,9 +1,14 @@
 var input = document.getElementById("srcPoke");
+var output = document.querySelector(".srcBg");
 
 
-input.addEventListener("focus", ()=> {
-    
-    let output = document.querySelector(".srcBg");
-    
+input.addEventListener("focus", () => {
     output.style.backgroundColor = "var(--red)";
 });
+
+input.onkeydown =  (event) =>  
+{if (event.key === "Enter") document.querySelector(".pokeball").classList.add("pokeball-roll")
+;}
+
+
+
