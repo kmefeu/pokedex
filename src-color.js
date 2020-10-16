3f710911bae85699c4051bc4cfafe3f2bcaf31ef
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-10-16 11:30:46 -0300

    Day#3 Up#1

--- src-color.js
+++ src-color.js
@@ -6,6 +6,11 @@ input.addEventListener("focus", () => {
     output.style.backgroundColor = "var(--red)";
 });
 
+input.addEventListener("focusout", () => {
+    output.style.backgroundColor = "var(--midGray)";
+});
+
+
 input.onkeydown =  (event) =>  
 {if (event.key === "Enter") document.querySelector(".pokeball").classList.add("pokeball-roll")
 ;}
