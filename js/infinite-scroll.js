00d468fda4b2e3fc5d05dac23e3f472350bfc83e
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-10-29 17:50:33 -0300

    Day#9 Up#2

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -1,5 +1,17 @@
-window.onscroll = function() {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-     loadMore().then(async () => await loadPoke());
+window.onscroll = function () {
+
+  async function paratudo() {
+
+      if ((window.innerHeight + window.scrollY + 1) >= document.body.offsetHeight) {
+      console.log("entrou")
+
+     await loadMore().then(async () => await loadPoke());
+
+    }
   }
+  paratudo()
+  // setTimeout(()=>{ console.log("parou") }, 1000);
 };
+
+
+// colocar uma função dentro da outra com await ao invés de set time out ???
