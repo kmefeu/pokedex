f6e5808a90643bdf597f5d8fa621f9d0f958a392
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-11-02 20:55:39 -0300

    Day#10 Up#1 - Layout Scale

--- js/add-preview.js
+++ js/add-preview.js
@@ -13,21 +13,3 @@ function addSrcPreview(i, name) {
     document.querySelector(".preview").appendChild(div);
 
 }
-
-function addSrcPreview2(i, name) {
-
-
-    let div = document.createElement("div");
-
-    let previewHtml = ` 
-   <div class="srcPreview" value="${i + 1}">
-   <p class="hach">#</p>
-   <p class="idPreview">${i + 1}<p/>
-   <p class="namePreview">${name}</p>
-   </div>`
-
-    div.innerHTML = previewHtml
-
-    document.querySelector(".preview").appendChild(div);
-
-}
