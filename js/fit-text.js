fbea6615c0f0384aab8a587b35c9b8d6546c8197
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-11-09 16:21:15 -0300

    Day#14 Up#2 - initial responsiveness

--- js/fit-text.js
+++ js/fit-text.js
@@ -2,7 +2,7 @@ function typeResize(name) {
 
     let size = "";
 
-    if (name.length > 9) { size = "font-size:50px" }
+    if (name.length >= 9) { size = "font-size:50px" }
     if (name.length > 10) { size = "font-size:48px" }
     if (name.length > 11) { size = "font-size:42px" }
     if (name.length > 12) { size = "font-size:38px" }
@@ -17,12 +17,12 @@ function typeResize2(name) {
 
     let size = "";
 
-    if (name.length > 8) { size = "font-size:6.85vw" }
-    if (name.length > 10) { size = "font-size:6.75vw" }
-    if (name.length > 11) { size = "font-size:6.65vw" }
-    if (name.length > 12) { size = "font-size:6.5vw" }
-    if (name.length > 13) { size = "font-size:6vw" }
-    if (name.length > 14) { size = "font-size:5.5vw" }
+    if (name.length >= 8) { size = "font-size:6.85vw" }
+    if (name.length >= 10) { size = "font-size:6.75vw" }
+    if (name.length >= 11) { size = "font-size:6.65vw" }
+    if (name.length >= 12) { size = "font-size:6.5vw" }
+    if (name.length >= 13) { size = "font-size:6vw" }
+    if (name.length >= 14) { size = "font-size:5.5vw" }
 
     return size
 }
