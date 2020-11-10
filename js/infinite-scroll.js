fe08d632824b127e0c473557dad86c5bfc3428f8
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-11-10 16:42:23 -0300

    Day#14 Up#10 - Loading Animation

--- js/infinite-scroll.js
+++ js/infinite-scroll.js
@@ -1,5 +1,8 @@
 window.addEventListener("scroll",infiniteScroll)
 async function infiniteScroll() {
   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-      await loadMore()
+    document.querySelector(".pokeball").classList.add("roll")
+    document.querySelector("#srcPoke").value = "Loading...";  
+    await loadMore()
+    
   }}
