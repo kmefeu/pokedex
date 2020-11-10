4a6b467961ca900d9bea4e9f83b8eac98ac034f6
Matheus Corrêa <67432473+kmefeu@users.noreply.github.com>
2020-11-10 14:13:13 -0300

    Day#14 Up#3 - Mobile Responsiveness

--- js/fit-text.js
+++ js/fit-text.js
@@ -31,7 +31,15 @@ function typeResize2(name) {
 
     let size = "";
 
+    if (window.innerWidth > 1200) {
 
+        if (name.length >= 8) { size = "font-size:6.85vw" }
+        if (name.length >= 10) { size = "font-size:6.75vw" }
+        if (name.length >= 11) { size = "font-size:6.65vw" }
+        if (name.length >= 12) { size = "font-size:6.5vw" }
+        if (name.length >= 13) { size = "font-size:6vw" }
+        if (name.length >= 14) { size = "font-size:5.5vw" }
+    }
 
     if (window.innerWidth <= 1200) {
 
@@ -43,23 +51,23 @@ function typeResize2(name) {
         if (name.length > 14) { size = "font-size:60px" }
 
     }
-    else if (window.innerWidth <= 550){
-        
-        if (name.length >= 9) { size = "font-size:72px" }
-        if (name.length > 10) { size = "font-size:68px" }
-        if (name.length > 11) { size = "font-size:64px" }
-        if (name.length > 12) { size = "font-size:60px" }
-        if (name.length > 13) { size = "font-size:56px" }
-        if (name.length > 14) { size = "font-size:52px" }
+    if (window.innerWidth <= 550) {
+
+        if (name.length >= 9) { size = "font-size:70px" }
+        if (name.length > 10) { size = "font-size:66px" }
+        if (name.length > 11) { size = "font-size:62px" }
+        if (name.length > 12) { size = "font-size:58px" }
+        if (name.length > 13) { size = "font-size:54px" }
+        if (name.length > 14) { size = "font-size:50px" }
     }
-    else {
-        
-        if (name.length >= 8) { size = "font-size:6.85vw" }
-        if (name.length >= 10) { size = "font-size:6.75vw" }
-        if (name.length >= 11) { size = "font-size:6.65vw" }
-        if (name.length >= 12) { size = "font-size:6.5vw" }
-        if (name.length >= 13) { size = "font-size:6vw" }
-        if (name.length >= 14) { size = "font-size:5.5vw" }
+    if (window.innerWidth <= 480) {
+
+        if (name.length >= 9) { size = "font-size:52px" }
+        if (name.length > 10) { size = "font-size:48px" }
+        if (name.length > 11) { size = "font-size:44px" }
+        if (name.length > 12) { size = "font-size:40px" }
+        if (name.length > 13) { size = "font-size:36px" }
+        if (name.length > 14) { size = "font-size:32px" }
     }
 
     return size
